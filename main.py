bef51c39090cd97efe4e4efcf8f5ce75ab679460
aaronEberhart <aaron.eberhart@gmail.com>
2019-01-29 16:56:54 -0500

    ...

--- main.py
+++ main.py
@@ -24,11 +24,14 @@ def formatStatistics(start,gen,reas,neg):
 	              len(reas.knownCType1)+len(reas.knownCType3),reasStats[0][1][1],reasStats[0][2][1],reasStats[1][1][1],reasStats[1][2][1],reasStats[2][1][1],reasStats[2][2][1], \
 	              len(neg.notCType1)+len(neg.notCType2)+len(neg.notCType3)+len(neg.notCType4)+len(neg.notRoleChains)+len(neg.notRoleSubs),negStats[0][1][1],negStats[0][2][1],negStats[1][1][1],negStats[1][2][1],negStats[2][1][1],negStats[2][2][1])
 
+def keepTrying(listy,y):
+	return not any(x.consequent.name == y for x in listy)
+
 def writeFileI(i,generator,reasoner,reasonerSteps,negatives,start):
 	if not os.path.isdir("output/{}".format(i)): os.mkdir("output/{}".format(i))
 	if not os.path.isdir("output/{}/sequence".format(i)): os.mkdir("output/{}/sequence".format(i))
 	if not os.path.isdir("output/{}/KB during sequence".format(i)): os.mkdir("output/{}/KB during sequence".format(i))
-	if not os.path.isdir("output/{}/KB after sequence".format(i)): os.mkdir("output/{}/KB after sequence".format(i))
+	if len(reasoner.KBaLog) > 0 and not os.path.isdir("output/{}/KB after sequence".format(i)): os.mkdir("output/{}/KB after sequence".format(i))
 	writeFile("owl/{}funcSynt.owl".format(i),reasoner.toFunctionalSyntax("<http://www.randomOntology.com/not/a/real/IRI/>"))
 	for j in range(0,len(reasoner.sequenceLog)):
 		writeFile("output/{}/sequence/reasonerStep{}.txt".format(i,j),reasoner.getSequenceLogI(j))
@@ -38,7 +41,7 @@ def writeFileI(i,generator,reasoner,reasonerSteps,negatives,start):
 		if len(reasoner.KBaLog[j]) > 0: writeFile("output/{}/KB after sequence/reasonerStep{}.txt".format(i,j+len(reasoner.sequenceLog)),reasoner.getKBaLogI(j))
 	writeFile("output/{}/completedKB.txt".format(i),generator.toString()+reasoner.toString()+negatives.toString())
 	writeFile("output/{}/completedReasonerDetails.txt".format(i),formatStatistics(start,generator,reasoner,negatives)+reasoner.getRuleCountString()+reasoner.getLog()+reasonerSteps.toString())	
-	if len(reasoner.KBaLog) < 1: print("after error")
+	#if len(reasoner.KBaLog) < 1: print("after error")
 	if len(reasoner.sequenceLog) != 40: print("seq error")
 
 def runExperiment(i,diff):
@@ -51,13 +54,15 @@ def runExperiment(i,diff):
 		
 		start = time.time()
 		
-		generator = HardGenERator(rGenerator=GenERator(numCType1=50,numCType2=50,numCType3=50,numCType4=50,numRoleSub=20,numRoleChains=20,conceptNamespace=100,roleNamespace=20),difficulty=diff)
+		generator = HardGenERator2(rGenerator=GenERator(numCType1=25,numCType2=25,numCType3=25,numCType4=25,numRoleSub=10,numRoleChains=10,conceptNamespace=100,roleNamespace=20),difficulty=diff)
 		
-		reasoner = ReasonER(generator,showSteps=True)	
+		generator.genERate()
 		
-		reasonerSteps = StepFindER(reasoner)
+		tryAgain = keepTrying(generator.CType1,generator.rGenerator.conceptNamespace-1)
+		    
+	reasoner = ReasonER(generator,showSteps=True)	
 		
-		tryAgain = len(reasoner.KBaLog) == 0
+	reasonerSteps = StepFindER(reasoner)
 	
 	negatives = NegativesGenERator(reasoner)
 	
