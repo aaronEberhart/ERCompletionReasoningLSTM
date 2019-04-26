6e4ae344c4a9d80da1940c13b7a76eee20a09908
aaronEberhart <aaron.eberhart@gmail.com>
2019-04-26 13:32:28 -0400

    trying to make it run...

--- seq2seq.py
+++ seq2seq.py
@@ -85,7 +85,7 @@ print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 
 n_epochs = 2
 train_size = X_train.shape[0]
-n_neurons = X_train.shape[0]
+n_neurons = y_train.shape[2]# * X_train.shape[2]
 n_layers = 1
 
 X = tf.placeholder(tf.float32, shape=X_train.shape)
@@ -95,6 +95,7 @@ basic_cell = tf.contrib.rnn.LSTMCell(num_units=n_neurons,use_peepholes=True)
 multi_layer_cell = tf.contrib.rnn.MultiRNNCell([basic_cell] * n_layers)
 outputs, states = tf.nn.dynamic_rnn(multi_layer_cell, X, dtype=tf.float32)
 
+
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
@@ -102,6 +103,7 @@ with tf.Session() as sess:
     for epoch in range(n_epochs):
         for iteration in range(train_size):    
             sess.run(outputs,feed_dict={X: X_train,y: y_train})
+            print(outputs)
         acc_train = accuracy.eval(feed_dict={X: X_train, y: y_train})
         acc_test = accuracy.eval(feed_dict={X: X_test, y: y_test})
         print(epoch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)        
